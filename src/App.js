e538a2f084ff0aee234fae300a5a84036c36f37f
Agboola Feyikemi <agboolafeyikemi@YV-Feyikemi.local>
2022-06-29 20:33:01 +0100

    Genesis Commit

--- src/App.js
+++ src/App.js
@@ -1,18 +1,80 @@
 import "./styles/App.css";
 import twitterLogo from "./assets/twitter-logo.svg";
 import React, { useEffect, useState } from "react";
+import { ethers } from "ethers";
+import myEpicNft from "./utils/MyEpicNFT.json";
 
 // Constants
-const TWITTER_HANDLE = "_buildspace";
+const TWITTER_HANDLE = "FeyikemiAgbool1";
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 const OPENSEA_LINK = "";
 const TOTAL_MINT_COUNT = 50;
 
 const App = () => {
   const [currentAccount, setCurrentAccount] = useState("");
+  const [loader, setLoader] = useState(false);
+  const CONTRACT_ADDRESS = "0x8Fc690b01232A2D10d6004C421B181bbaa3BAeD9";
+  // Setup our listener.
+  const setupEventListener = async () => {
+    // Most of this looks the same as our function askContractToMintNft
+    try {
+      const { ethereum } = window;
 
+      if (ethereum) {
+        // Same stuff again
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const connectedContract = new ethers.Contract(
+          CONTRACT_ADDRESS,
+          myEpicNft.abi,
+          signer
+        );
+
+        // THIS IS THE MAGIC SAUCE.
+        // This will essentially "capture" our event when our contract throws it.
+        // If you're familiar with webhooks, it's very similar to that!
+        connectedContract.on("NewEpicNFTMinted", (from, tokenId) => {
+          console.log(from, tokenId.toNumber());
+          alert(
+            `Hey there! We've minted your NFT and sent it to your wallet. It may be blank right now. It can take a max of 10 min to show up on OpenSea. Here's the link: https://testnets.opensea.io/assets/${CONTRACT_ADDRESS}/${tokenId.toNumber()}`
+          );
+        });
+
+        console.log("Setup event listener!");
+      } else {
+        console.log("Ethereum object doesn't exist!");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
   const askContractToMint = async () => {
-    const CONTRACT_ADDRESS = "0x86b713E4FAdB7d86F134B9C42aF19c19D22b64Ba";
+    try {
+      const { ethereum } = window;
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const connectedContract = new ethers.Contract(
+          CONTRACT_ADDRESS,
+          myEpicNft.abi,
+          signer
+        );
+        console.log("Going to pop wallet now to pay gas...");
+        let nftTxn = await connectedContract.makeAnEpicNFT();
+        console.log("Mining...please wait");
+        setLoader(true);
+        await nftTxn.wait();
+
+        console.log(
+          `Mined, see transaction: https://rinkeby.etherscan.io/tx/${nftTxn.hash}`
+        );
+        setLoader(false);
+      } else {
+        console.log("Ethereum object does not exist!");
+      }
+    } catch (error) {
+      console.log("error", error);
+    }
   };
 
   const checkIfWallectIsConnected = async () => {
@@ -29,6 +91,9 @@ const App = () => {
       const account = accounts[0];
       console.log("Found an authorized account", account);
       setCurrentAccount(account);
+      // Setup listener! This is for the case where a user comes to our site
+      // and ALREADY had their wallet connected + authorized.
+      setupEventListener();
     } else {
       console.log("No authorized account found");
     }
@@ -58,6 +123,9 @@ const App = () => {
        */
       console.log("Connected", accounts[0]);
       setCurrentAccount(accounts[0]);
+      // Setup listener! This is for the case where a user comes to our site
+      // and connected their wallet for the first time.
+      setupEventListener();
     } catch (error) {
       console.log(error);
     }
@@ -71,6 +139,7 @@ const App = () => {
       Connect to Wallet
     </button>
   );
+
   useEffect(() => {
     checkIfWallectIsConnected();
   }, []);
@@ -83,7 +152,29 @@ const App = () => {
           <p className="sub-text">
             Each unique. Each beautiful. Discover your NFT today.
           </p>
-          {renderNotConnectedContainer()}
+          <p className="footer-text">
+            1 / {TOTAL_MINT_COUNT} NFTs minted so far
+          </p>
+          <p className="footer-text">
+            <a
+              className="mt"
+              href="https://testnets.opensea.io/collection/squarenft-dg2jlmysn4"
+              target="_blank"
+            >
+              this
+            </a>
+            is my collection.
+          </p>
+          {currentAccount === "" ? (
+            renderNotConnectedContainer()
+          ) : (
+            <button
+              onClick={askContractToMint}
+              className="cta-button connect-wallet-button"
+            >
+              {loader ? "Loading..." : "Mint NFT"}
+            </button>
+          )}
         </div>
         <div className="footer-container">
           <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
